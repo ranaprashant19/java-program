648aa4dac5a7b011ee2f2198eebf127f81b370c9
Prashant Rana <101080988+ranaprashant19@users.noreply.github.com>
2025-06-21 23:01:31 +0530

    Create AsciiValue.java
    Print Ascii Value from user input

---
+++ AsciiValue.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AsciiValue{
+
+  static void printAscii(char c){
+    System.out.println("ASCII value of "+c+"is "+(int)c);
+  }
+
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the character: ");
+    char ch=sc.next().CharAt(0);
+    printAscii(ch);
+  }
+}
