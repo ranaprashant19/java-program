e415cc4cb185879a581b00c28ba933e54d0f3e45
Prashant Rana <101080988+ranaprashant19@users.noreply.github.com>
2025-06-21 23:10:43 +0530

    Create AsciiValue.java

--- AsciiValue.java
+++ AsciiValue.java
@@ -9,7 +9,7 @@ public class AsciiValue{
   public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter the character: ");
-    char ch=sc.next().CharAt(0);
+    char ch=sc.next().charAt(0);
     printAscii(ch);
   }
 }
