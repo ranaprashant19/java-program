d3e19f54041da8c151c146d6c94d836ffc9cc7b7
Prashant Rana <101080988+ranaprashant19@users.noreply.github.com>
2025-06-30 22:46:32 +0530

    Create FloatToInt.java

--- FloatToInt.java
+++ FloatToInt.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class FloatToInt{
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter the demincal number: ");
         float f = sc.nextFloat();
         int i = (int) f;
         System.out.println("The float number is " + f + "integer " + i);
